ffbc9b096b77d61c59fa4494091a13eedd5b2a31
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-06-12 16:22:55 +0100

    Méthodes utilisées pour la liaison de données : Comment ça marche

--- app.js
+++ app.js
@@ -8,8 +8,16 @@ const app = createApp({
     };
   },
   methods: {
-    setName(event, lastName) {
-      this.name = event.target.value; // + ' ' + lastName;
+    outputFullName() {
+      // console.log('running again...') // les methodes ne sont pas la meilleur solution 
+      // pour générer une valeur calculée dynamiquement comme celle-ci.
+      if (this.name === "") {
+        return;
+      }
+      return this.name + ' ' + 'Mobile/Web Dev';
+    },
+    setName(event) {
+      this.name = event.target.value;
     },
     add(num) {
       this.counter = this.counter + num;
