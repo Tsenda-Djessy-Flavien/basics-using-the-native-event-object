934ac8973e8496367b61e43843df809f2bd3a9ae
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-06-13 13:50:08 +0100

    Travailler avec Watchers

--- app.js
+++ app.js
@@ -4,27 +4,54 @@ const app = createApp({
   data() {
     return {
       counter: 0,
-      name: "",
+      name: '',
+      lastName: '',
+      // fullName: '',
     };
   },
+  // function qui est executer par Vue lorsque ses dependances changes
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        this.counter = 0;
+      }
+    },
+    // à chaque fois que la proprièté(variable) name change, cette func name() observateur est réexécutée
+    // (newValue, oldValue)
+    // name(value) {
+    //   if (value === '') {
+    //     this.fullName = '';
+    //   } else {
+    //     this.fullName = value + ' ' + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === '') {
+    //     this.fullName = '';
+    //   } else {
+    //     this.fullName = this.name + ' ' + value;
+    //   }
+    // },
+  },
+  // Pour la sortie de value dynamic qui dependent de plusieurs values d'entrée
   computed: {
     fullName() {
-      console.log('running again...')
-      if (this.name === "") {
-        return;
+      console.log("running again...");
+      if (this.name === "" || this.lastName === "") {
+        return '';
       }
-      return this.name + ' ' + 'Mobile/Web Dev';
-    }
+      return this.name + " " + this.lastName;
+    },
   },
   // utiliser les methodes uniquement pour calculer les values ou pour declencher les events
   methods: {
     outputFullName() {
-      // console.log('running again...') // les methodes ne sont pas la meilleur solution 
+      // console.log('running again...') // les methodes ne sont pas la meilleur solution
       // pour générer une valeur calculée dynamiquement comme celle-ci.
       if (this.name === "") {
         return;
       }
-      return this.name + ' ' + 'Mobile/Web Dev';
+      return this.name + " " + "Mobile/Web Dev";
     },
     setName(event) {
       this.name = event.target.value;
@@ -38,6 +65,7 @@ const app = createApp({
     },
     resetInput() {
       this.name = "";
+      this.lastName = "";
     },
   },
 });
